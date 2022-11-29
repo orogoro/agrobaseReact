5cf8fcec7b0c07ea4b70ba7b414c256dd5bf809e
Yurii Dvornichenko <raen98@live.ru>
2022-11-29 13:26:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="agrobaseReact">
         <App />
       </BrowserRouter>
     </Provider>
